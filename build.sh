0f5280da70d64e987be12af73b7681239bce7f65
G2-Games <ke0bhogsg@gmail.com>
2023-10-23 17:34:06 -0500

    Added simple build script and gitignore

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
- 
+version="2.1.2"
+currentdir=$(pwd)
+
+mkdir -p build/
+
+cd build/ || exit 1
+
+rm *
+exa -lh
+
+7z a -tzip "Old.Ores.$version-Java.zip" ../Java/*
+7z a -tzip "Old.Ores.$version-Bedrock.mcpack" ../Bedrock/*
+
+cd "$currentdir" || exit 1
